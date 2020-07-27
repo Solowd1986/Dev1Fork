9c7dc3e17b17f76e3ec920ddb5c070883171522d
Admin <Admin@ya.com>
2020-07-27 19:12:28 +0300

    master

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,6 +15,28 @@ excel.render();
 
 
 
+new Promise((res, rej) => {
+    setTimeout(() => {
+        console.log("start");
+        rej("error handle progress");
+    }, 2000)
+}).catch((error) => {
+    console.log(error);
+    return "error handled success";
+}).then((msg) => {
+    return new Promise((res) => {
+        setTimeout(() => {
+            console.log(msg);
+            res();
+        }, 2000);})
+}).finally(() => {
+    console.log("done");
+});
+
+
+
+
+
 
 
 
