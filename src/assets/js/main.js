7388e3d6ddf713301138f5896f6193bb629f3e5e
Admin <Admin@ya.com>
2020-09-13 02:01:51 +0300

    webpack new

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,46 +8,91 @@ require.context('../img/', true, /\.jpe?g$|.png$|.svg$|.gif$/);
 
 import {cnt} from "./data";
 import info from "./data";
-console.log(cnt);
-console.log(info.name);
 
-const bd = () => {
-    console.log(this);
+
+const data = {
+  phone: {
+      cnt: 12,
+      price: 1000
+  }
 };
 
-class Base {
-    constructor() {
-        this.data = 12;
+
+class Component {
+    constructor(props) {
+        this.props = props;
     }
-    show() {
-        console.log(this.data);
-    }
-    list(elem) {
-        let foo = this.show.bind(this);
-        elem.addEventListener("click", function (evt) {
-            console.log(this);
-            bd();
-        });
+
+    render() {
+
     }
 }
 
-const d = new Base();
-d.list(document.querySelector("div"));
-
-
-const score = {
-    name: "Bob",
-    age: 21
+const props = {
+    range: ".range-quantity"
 };
 
-let str = "<h1>{{name}}</h1><p>{{age}}</p>";
-let res = str.replace(/{{(.*?)}}/g, (match, data) => {
-    console.log(match);
-    console.log(data);
-    return score[data];
-});
+function f2() {
+    const range = document.querySelector(".range-quantity");
+    const rangeParent = document.querySelector(".range-quantity").parentElement;
+    let rangeElem = null;
+    range.addEventListener("change", function (evt) {
+        if (rangeElem !== null) {
+            return;
+        }
+        rangeElem = document.createElement("input");
+        rangeElem.type = "range";
+        rangeElem.min = "1";
+        rangeElem.max = "6";
+        rangeParent.appendChild(rangeElem);
+        console.log(this.value);
+    });
+}
 
-console.log("res - ", res);
+f2();
+
+
+const base = new Component(props);
+
+
+function f3() {
+    return document.createElement("div").classList.add("ss");
+}
+
+
+console.log(f3());
+
+
+//document.querySelector("body").appendChild(f3());
+
+
+
+
+
+async function f1() {
+    await new Promise(resolve => {
+        setTimeout(() => {
+            console.log("st");
+            resolve();
+            }, 2000);
+    });
+    console.log("done");
+    return "all stuff";
+}
+
+//f1().then(data => console.log(data));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
