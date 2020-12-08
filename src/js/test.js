b97095ee2d98bd09d0d62a740001903673b2c9bf
Admin <Admin@ya.com>
2020-12-08 15:02:36 +0300

    stuff

---
+++ src/js/test.js
@@ -0,0 +1,37 @@
+import "normalize.css";
+import "../assets/scss/main.scss";
+import "@babel/polyfill";
+// require.context('../assets/img/', true, /\.jpe?g$|.png$|.svg$|.gif$/);
+
+import { DomHelper } from "./components/helpers/DomHelper";
+import { Request } from "./components/fetch/Request";
+
+import { AdminInitPanel } from "./components/admin/AdminInitPanel";
+import { CookieHelper } from "./components/helpers/CookieHelper";
+
+const name = 12;
+
+function sell(name) {
+  console.log(name);
+}
+
+const gg = "12";
+
+fetch("api/src")
+  .then((res) => res.text()
+    .then((res) => console.log(res)));
+
+// fetch("/api/get")
+//     .then((res) =>
+//         res.text()
+//             .then(res => console.log(res)));
+
+new AdminInitPanel({
+  shomListOfTablesBtn: ".show-tables",
+  listOfTablesWrapper: ".admin__list-of-sections",
+  listOfRecordsWrapper: ".admin__records-wrapper",
+});
+
+const app = document.querySelector(".app");
+
+app.innerHTML = "APP";
